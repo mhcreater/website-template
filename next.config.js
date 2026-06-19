f9fee0613f826f3ad8c9fc2f04a66d4442259b80
MH Creater <hemanthbcom20@gmail.com>
2026-06-19 13:15:02 +0530

    Fix GitHub Pages imagFix image paths for GitHub Pagese paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  basePath: "/website-template",
+  assetPrefix: "/website-template/",
 };
 
 module.exports = nextConfig;
